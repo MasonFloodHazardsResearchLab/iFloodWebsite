91553145ce566322da3145d83975681cf1da7f28
Arslaan Khalid <akhalid6@gmu.edu>
2019-08-26 13:03:04 -0400

    added hints for using the harp on Linux

--- upload.bat
+++ upload.bat
@@ -1,3 +1,3 @@
-call harp compile
+call harp compile # when using this on the LINUX machine use "harp compile" instead
 aws s3 sync ./www s3://gmu-iflood-interface --delete
 aws cloudfront create-invalidation --distribution-id E1Z7PVBPK4FC81 --paths /*
